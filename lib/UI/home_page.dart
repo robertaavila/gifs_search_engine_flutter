9cb50ed4b7a862c72f873086ca376d9a50bed347
Roberta Avila <rvc.avila@gmail.com>
2021-06-03 19:09:12 -0300

    inserindo o botão "carregar mais", que aparece quando uma busca é feita, no fim da tela.

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -19,10 +19,10 @@ class _HomePageState extends State<HomePage> {
 
     if (_search == null) {
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/trending?api_key=1MlveLa9mPVcUtjwM7yvB0Q1bnYSxYY6&limit=25&rating=g");
+          "https://api.giphy.com/v1/gifs/trending?api_key=1MlveLa9mPVcUtjwM7yvB0Q1bnYSxYY6&limit=10&rating=g");
     } else {
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=1MlveLa9mPVcUtjwM7yvB0Q1bnYSxYY6&q=$_search&limit=25&offset=$_offset&rating=g&lang=pt");
+          "https://api.giphy.com/v1/gifs/search?api_key=1MlveLa9mPVcUtjwM7yvB0Q1bnYSxYY6&q=$_search&limit=9&offset=$_offset&rating=g&lang=pt");
     }
     return json.decode(response.body);
   }
@@ -62,6 +62,12 @@ class _HomePageState extends State<HomePage> {
               ),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
+              onSubmitted: (text){
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
             ),
           ),
           Expanded(
@@ -94,20 +100,49 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  int _getCount(List data){
+    if(_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
       padding: EdgeInsets.all(10.0),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
-      itemCount: snapshot.data["data"].length,
+      itemCount: _getCount(snapshot.data["data"]),
       itemBuilder: (context, index) {
-        return GestureDetector(
-          child: Image.network(
-            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-            height: 300.0,
-            fit: BoxFit.cover,
-          ),
-        );
+        if(_search == null || index < snapshot.data["data"].length){
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        } else {
+          return Container(
+            child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.add, color: Colors.white, size: 70.0,),
+                  Text("Carregar mais...",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                ],
+              ),
+              onTap: (){
+                setState(() {
+                  _offset += 7;
+                });
+              },
+            )
+          ) ;
+        }
       },
     );
   }
